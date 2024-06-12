35bf4735346014fbc79782b33d915328c04470b1
Alessandro <duccialessandro001@gmail.com>
2024-06-12 13:19:30 +0400

    Added pictures, created visuals for design 1: nav-bar, logo and presentation of services with background image as landing page. No links yet.

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,32 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      
+      <div>
+        <header>
+          <section className="header-section-options text-jetblack">
+            <div>About Us</div>
+            <div>Our Services</div>
+            <div>More</div>
+          </section>
+        </header>
+        <div className="landing-screen background-whitesmoke">
+          <div className="blur">
+          </div>
+          <div className="logo-bg-square">
+            <div className="logo-center"></div>
+          </div>
+            <div className="presentation-left text-jetblack">
+              <div></div>
+              <div></div>
+            </div>
+            <div className="presentation-middle">
+
+            </div>
+            <div className="presentation-right">
+
+            </div>
+        </div>
+      </div>
     </main>
   );
 }
