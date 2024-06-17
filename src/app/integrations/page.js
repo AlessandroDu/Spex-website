dae4fbb36aa66d143d62dd96f3160cef260fb0ba
Alessandro <duccialessandro001@gmail.com>
2024-06-17 16:55:02 +0400

    Cleaned up existing code, continue to work on main design now. Landing screen done, integrations landing screen done, home div 1 done, now working on integrations div 1 design. Forgot to make new branches for new features.

--- src/app/integrations/page.js
+++ src/app/integrations/page.js
@@ -9,15 +9,14 @@ export default function Integrations() {
     return (
         <div>
             <div className="landing-screen integrations-screen">
-                {/* <div className="blur-integrations"></div> */}
-                <div className="landing-screen-top-half-integrations">
-                    <Link className='link' href="/design-1c">
+                <div className="landing-screen-title-integrations">
+                    <Link className='link' href="/">
                         <div className="logo-bg-square-integrations">
                           <div className="logo-center-integrations"></div>
                         </div>
                     </Link>
                     <div className="title-integrations">
-                        <h2 className='text-jetblack-integrations'>Integrations</h2>
+                        <h2 className='text-jetblack text-xl'>Integrations</h2>
                     </div>
                 </div>
                 <div className='see-more-container-integrations'>
@@ -31,10 +30,39 @@ export default function Integrations() {
                      </Link>
                 </div>
             </div>
-            <div id='div1' className='div1-integrations'>
-                <h1 className='what-we-offer-header-integrations'>What we offer</h1>
-                <div className='div1-subdiv-integrations'>
-
+            <div id='div1' className='div-1'>
+                <h1 className='div-1-header'>Acoustic</h1>
+                <div className='div-1-subdiv'>
+                  <div className='div-1-text'>
+                    <h1>Our Expertise</h1>
+                    <br/>
+                    <p>
+                    Our Company has a solid expertise in acoustic design and implementation,<br/>
+                    our capabilities in creating optimal spaces for your installations.<br/><br/>
+                    With a focus on precision, innovation, and attention to detail,<br/>
+                    we developed a comprehensive understanding of the complex<br/>
+                    interplay between acoustics, architecture and technology.
+                    </p>
+                    <br/>
+                    <h1>Our services</h1>
+                    <p>- Acoustic design and analysis for permanent installations</p>
+                    <p>- Room acoustics, sound reinforcement, and audio system design</p>
+                    <p>- Measurement and optimization of sound quality</p>
+                    <p>- Integration with architectural and structural design elements</p>
+                    <p>- Project management and coordination with clients and contractors</p>
+                  </div>
+                  <div className='div-1-portfolio-container'>
+                    <div className='div-1-portfolio-subcontainer'>
+                      <div className='div-1-pics pic-normal pic1-integrations-acoustic'></div>
+                      <div className='div-1-pics pic-normal pic2-integrations-acoustic'></div>
+                      <div className='div-1-pics pic-normal pic3-integrations-acoustic'></div>
+                    </div>
+                    <div className='div-1-portfolio-subcontainer'>
+                      <div className='div-1-pics pic-xl pic4-integrations-acoustic'></div>
+                      <div className='div-1-pics pic-normal pic5-integrations-acoustic'></div>
+                      <div className='div-1-pics pic-normal pic6-integrations-acoustic'></div>
+                    </div>
+                  </div>
                 </div>
             </div>
         </div>
--- src/app/page.js
+++ src/app/page.js
@@ -4,29 +4,76 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <div className="landing-screen">
-        <div className="blur">
+            <div className="landing-screen">
+              <div className="blur">
+              </div>
+              <div className="landing-screen-top-half">
+                <Link href="/">
+                    <div className="logo-bg-square">
+                      <div className="logo-center"></div>
+                    </div>
+                </Link>
+                <div className="details-1c">
+                    <div className="details-left text-whitesmoke">office@spex.mu</div>
+                    <Link className="details-middle text-whitesmoke" href="/#about-us">
+                      <div>About us</div>
+                    </Link>
+                    <div className="details-right text-whitesmoke">+230 5453 5710</div>
+                </div>
+              </div>
+              <div className="presentations-container-1c">
+                <div>
+                  <div className="presentation-left-1c">
+                    <div className="header-left-1c"><p className="header-p">Technology</p></div>
+                  </div>
+                </div>
+                <div>
+                  <Link href="/integrations">
+                    <div className="presentation-middle-1c">
+                      <div className="header-middle-1c"><p className="header-p">Integrations</p></div>
+                    </div>
+                  </Link>
+                </div>
+                <div>
+                  <div className="presentation-right-1c">
+                    <div className="header-right-1c"><p className="header-p">Energy</p></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div id='about-us' className='div-1'>
+              <h1 className='div-1-header'>About Us</h1>
+                <div className='div-1-subdiv'>
+                  <div className='div-1-text'>
+                    <h1>Welcome to SPEX Services Ltd</h1>
+                    <br/>
+                    <p>
+                    A leading integration solutions provider based in Mauritius, established in 2007.<br/><br/>
+
+                    With a strong focus on innovation, we specialize in the domains of audio, lighting, acoustic,<br/>
+                    smart intercoms, and automation.<br/><br/>
+
+                    Our expertise spans architectural, entertainment, and commercial applications, enabling us<br/>
+                    to deliver customized solutions that meet our clients' unique needs.<br/><br/>
+
+                    From conceptualization to implementation, we pride ourselves on our ability to integrate<br/>
+                    cutting-edge technology with exceptional design and customer service.
+                    </p>
+                  </div>
+                  <div className='div-1-portfolio-container'>
+                    <div className='div-1-portfolio-subcontainer'>
+                      <div className='div-1-pics pic-normal pic1-about-us'></div>
+                      <div className='div-1-pics pic-normal pic2-about-us'></div>
+                      <div className='div-1-pics pic-normal pic3-about-us'></div>
+                    </div>
+                    <div className='div-1-portfolio-subcontainer'>
+                      <div className='div-1-pics pic-xl pic4-about-us'></div>
+                      <div className='div-1-pics pic-normal pic5-about-us'></div>
+                      <div className='div-1-pics pic-normal pic6-about-us'></div>
+                    </div>
+                  </div>
+                </div>
+            </div>
         </div>
-        <div className="landing-screen-left-half">
-          <div className="logo-bg-square">
-            <div className="logo-center"></div>
-          </div>
-        </div>
-        <div className="presentations-container">
-          <div>
-            <div className="header-left"><p className="header-p">Software</p></div>
-            <div className="presentation-left"></div>
-          </div>
-          <div>
-            <div className="header-middle"><p className="header-p">Integrations</p></div>
-            <div className="presentation-middle"></div>
-          </div>
-          <div>
-            <div className="header-right"><p className="header-p">Solar</p></div>
-            <div className="presentation-right"></div>
-          </div>
-        </div>
-      </div>
-    </div>
   );
 }
