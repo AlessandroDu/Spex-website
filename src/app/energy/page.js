f48f0ed7d5976379c6040de799d9e6fcf1b5a633
Alessandro <duccialessandro001@gmail.com>
2024-06-20 01:15:03 +0400

    Worked on the energy landing screen, put a background video, now ready for some titles and more detailed presentations.

--- src/app/energy/page.js
+++ src/app/energy/page.js
@@ -8,7 +8,15 @@ import "../level2_styles.css";
 export default function Energy() {
     return (
         <div>
-            <div className="landing-screen integrations-screen">
+            <div className="landing-screen-energy">
+                <iframe 
+                    className='energy-landing-page-video' 
+                    src="https://player.vimeo.com/video/960291383?autoplay=1&amp;loop=1&amp;muted=1&amp;controls=0&amp;sidedock=0&amp;title=0&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" 
+                    frameborder="0"
+                    allow="autoplay; fullscreen"
+                    allowfullscreen="" 
+                    title="Sunology - Gamme produits - PLAY | PLAYMax | CITY | HOME">
+                </iframe>
                 <div className="landing-screen-title-integrations">
                     <Link className='link' href="/">
                         <div className="logo-bg-square-integrations">
@@ -22,12 +30,15 @@ export default function Energy() {
                 <div className='see-more-container-integrations'>
                      <Link className='see-more-btn' href="/integrations#acoustic">
                          <p>
-                            <span className='see-more-underline'>See more</span> <br/>
+                            <span className='see-more-underline'>See more</span><br/>
                              <IconButton className='btn-see-more' aria-label="unfold more">
                                <UnfoldMoreIcon fontSize="small"/>
                              </IconButton>
                          </p>
                      </Link>
+                </div>
+                <div className='landing-page-bottom-fade'>
+
                 </div>
             </div>
         </div>
